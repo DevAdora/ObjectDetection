1e0fd57f2f6fdeb4da37118f19cc02fae071690b
Rai M. Reyes Jr. <120080181+DevAdora@users.noreply.github.com>
2024-08-23 22:01:15 +0800

    Add files via upload

---
+++ objectdetection.py
@@ -0,0 +1,14 @@
+from gpiozero import DistanceSensor
+ultrasonic = DistanceSensor(echo = 17, trigger = 4, threshold_distance = 0.5)
+while True:
+    
+
+
+
+def in_range_5():
+    print("Object in 5 meters")
+    #Sound and LED input
+
+def in_range_1():
+    print("Object in 1 meter")
+    #Sound and LED input
